633e4c3722137e2e649bfa24f93cbc35a74e76db
fhemery <f.hemery@hemit.fr>
2024-02-22 09:57:11 +0100

    game of life: add helper

--- ts/src/08-game-of-life/game-of-life.spec.ts
+++ ts/src/08-game-of-life/game-of-life.spec.ts
@@ -1,10 +1,11 @@
 class Board {
     private _aliveCells = new Set<string>();
+
     constructor(private nbLines: number, private nbCols: number) {
 
     }
 
-    setAliveAt(x:number, y: number): void {
+    setAliveAt(x: number, y: number): void {
         this._aliveCells.add(`${x}:${y}`);
     }
 
@@ -14,7 +15,7 @@ class Board {
 
     toStringArray(): string[] {
         const result = [];
-        for (let i = 0; i< this.nbLines; i++ ){
+        for (let i = 0; i < this.nbLines; i++) {
             let line = '';
             for (let j = 0; j < this.nbCols; j++) {
                 if (this.isCellAliveAt(i, j)) {
@@ -44,23 +45,29 @@ class Board {
 
 class GameOfLife {
     private board: Board;
+
     constructor(private nbLines: number, private nbCols: number, ...lines: string[]) {
         this.board = new Board(nbLines, nbCols);
         for (let lineNb = 0; lineNb < nbLines; lineNb++) {
-            for (let columnNb= 0; columnNb < nbCols; columnNb++) {
-                if (lines[lineNb][columnNb] === '*'){
+            for (let columnNb = 0; columnNb < nbCols; columnNb++) {
+                if (lines[lineNb][columnNb] === '*') {
                     this.board.setAliveAt(lineNb, columnNb);
                 }
             }
         }
     }
+
     nextTurn(): void {
         const boardCopy = new Board(this.nbLines, this.nbCols);
         for (let lineNb = 0; lineNb < this.nbLines; lineNb++) {
-            for (let columnNb= 0; columnNb < this.nbCols; columnNb++) {
+            for (let columnNb = 0; columnNb < this.nbCols; columnNb++) {
+                const nbNeighbours = this.board.getNbNeighbours(lineNb, columnNb);
                 if (this.board.isCellAliveAt(lineNb, columnNb)) {
-                    const nbNeighbours = this.board.getNbNeighbours(lineNb, columnNb);
                     if (nbNeighbours === 2 || nbNeighbours === 3) {
+                        boardCopy.setAliveAt(lineNb, columnNb);
+                    }
+                } else {
+                    if (nbNeighbours === 3) {
                         boardCopy.setAliveAt(lineNb, columnNb);
                     }
                 }
@@ -74,6 +81,33 @@ class GameOfLife {
     }
 }
 
+interface BoardError {
+    x: number;
+    y: number;
+    expected: string;
+    actual: string;
+}
+
+function shouldMatch(board: string[], expectedBoard: string[]) {
+    const errors: BoardError[] = [];
+    for (let i = 0; i < expectedBoard.length; i++) {
+        for (let j = 0; j < expectedBoard[i].length; j++) {
+            if (expectedBoard[i][j] === '?') {
+                continue;
+            }
+            if (expectedBoard[i][j] !== board[i][j]) {
+                errors.push({
+                    x: i,
+                    y: j,
+                    expected: expectedBoard[i][j],
+                    actual: board[i][j]
+                });
+            }
+        }
+    }
+    expect(errors).toEqual([]);
+}
+
 describe('Game of life', () => {
     it('should return an empty grid when an empty grid is provided', () => {
         const game = new GameOfLife(2, 3, '...', '...');
@@ -84,16 +118,16 @@ describe('Game of life', () => {
     it('should not kill cell that has 2 or 3 neighbours', () => {
         const game = new GameOfLife(2, 3, '***', '...');
         game.nextTurn();
-        expect(game.getBoard()).toEqual(['.*.', '...']);
+        shouldMatch(game.getBoard(), ['?*?', '???']);
     })
 
     it('should spawn a cell that has exactly 3 neighbours', () => {
         const game = new GameOfLife(2, 3, '***', '...');
         game.nextTurn();
-        expect(game.getBoard()).toEqual(['???', '.*.']);
+        shouldMatch(game.getBoard(), ['???', '.*.']);
     })
 
-    xit('should kill any cell with less than 2 neighbours when an empty grid is provided', () => {
+    xit('should kill any cell with less than 2 neighbours', () => {
         const game = new GameOfLife(2, 3, '**.', '...');
         game.nextTurn();
         expect(game.getBoard()).toEqual(['...', '...']);
